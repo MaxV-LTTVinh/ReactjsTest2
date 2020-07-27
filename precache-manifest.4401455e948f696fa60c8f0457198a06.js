5074fa04f62c682bdf0017796505cf55173b2cd8
MaxV(Vinh) <44563184+MaxV-LTTVinh@users.noreply.github.com>
2020-07-27 16:46:28 +0700

    Updates

---
+++ precache-manifest.4401455e948f696fa60c8f0457198a06.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92962b18e8d0d619702622a9b648b76d",
+    "url": "/ReactjsTest2/index.html"
+  },
+  {
+    "revision": "946714b0ac8815984514",
+    "url": "/ReactjsTest2/static/css/main.8bb9b25a.chunk.css"
+  },
+  {
+    "revision": "0afd4dd5693f105eb710",
+    "url": "/ReactjsTest2/static/js/2.245eabea.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "/ReactjsTest2/static/js/2.245eabea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "946714b0ac8815984514",
+    "url": "/ReactjsTest2/static/js/main.65fa4193.chunk.js"
+  },
+  {
+    "revision": "50fce8775ff5cf93da66",
+    "url": "/ReactjsTest2/static/js/runtime-main.e79b7a87.js"
+  }
+]);
